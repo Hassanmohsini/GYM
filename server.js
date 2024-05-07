db411b2452c132c33274a1557daa3b39d960e2e9
Dany <danialikhani@gmail.com>
2024-05-07 13:42:31 +0200

    add cookieParser config

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ import colors from "colors";
 import connectDB from "./config/connectDB.js";
 import userRoutes from "./routes/user.route.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-const { PORT } = process.env;
+const { PORT, NODE_ENV } = process.env;
 
 await connectDB();
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/", userRoutes);
 
